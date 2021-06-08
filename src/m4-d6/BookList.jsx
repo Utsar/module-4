26f1098d281512f6cdc5434dfb340288e612112e
Kristian Utsar <78056935+Utsar@users.noreply.github.com>
2021-06-08 15:20:36 +0100

    commit

--- src/m4-d6/BookList.jsx
+++ src/m4-d6/BookList.jsx
@@ -1,38 +1,36 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import { Container, Row, Col, Form } from "react-bootstrap";
 import SingleBook from "./SingleBook";
 
-class BookList extends React.Component {
-  state = {
-    searchQuery: "",
-  };
-  render() {
-    return (
-      <Container>
-        <Row>
-          <Col>
-            <Form.Group controlId="formBasicEmail">
-              <Form.Label>Search</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="search herel"
-                value={this.state.searchQuery}
-                onChange={(e) => this.setState({ searchQuery: e.target.value })}
-              />
-            </Form.Group>
+const BookList = ({ FantasyBook }) => {
+  const [searchQuery, setSearchQuery] = useState("");
+  useEffect(() => {}, [FantasyBook]);
+
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Search</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="search herel"
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+            />
+          </Form.Group>
+        </Col>
+      </Row>
+      <Row>
+        {FantasyBook.filter((b) =>
+          b.title.toLowerCase().includes(searchQuery)
+        ).map((b) => (
+          <Col md={3}>
+            <SingleBook book={b} />
           </Col>
-        </Row>
-        <Row>
-          {this.props.FantasyBook.filter((b) =>
-            b.title.toLowerCase().includes(this.state.searchQuery)
-          ).map((b) => (
-            <Col md={3}>
-              <SingleBook book={b} />
-            </Col>
-          ))}
-        </Row>
-      </Container>
-    );
-  }
-}
+        ))}
+      </Row>
+    </Container>
+  );
+};
 export default BookList;
