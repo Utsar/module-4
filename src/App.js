70a1082e220fe487e494bad074620d82de7fa64c
Kristian Utsar <78056935+Utsar@users.noreply.github.com>
2021-06-09 14:42:11 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -7,21 +7,29 @@ import MyBadge from "./m4-d6/MyBadge";
 import SingleBook from "./m4-d6/SingleBook";
 import Fantasy from "./assets/fantasy.json";
 import BookList from "./m4-d6/BookList";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <MyNavBar title="Kristian's book store" />
-      <D2Jumbotron
-        title="Hello, welcome to my Book Store"
-        description="I have not read all those books but they look awesome!"
-      />
+      <Router>
+        <MyNavBar title="Kristian's book store" />
+        <D2Jumbotron
+          title="Hello, welcome to my Book Store"
+          description="I have not read all those books but they look awesome!"
+        />
 
-      <MyBadge text="Hello" color="warning" />
+        <Route path="/" exact component={MyBadge} />
+        {/* <MyBadge text="Hello" color="warning" /> */}
 
-      <WarningSign text="this is Warning Sign" />
-      <SingleBook book={Fantasy[0]} />
-      <BookList FantasyBook={Fantasy} />
+        <Route
+          path="/"
+          exact
+          render={() => <WarningSign text="this is Warning Sign" />}
+        />
+        <SingleBook book={Fantasy[0]} />
+        <BookList FantasyBook={Fantasy} />
+      </Router>
     </div>
   );
 }
